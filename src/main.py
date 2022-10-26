5dabfad4384b5b1771099d2617baf1ecf094f879
suxxez <coding@fentker.com>
2022-10-26 12:13:30 +0200

    Add openapi spec

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+import sys
+import json
 
 from fastapi import FastAPI
 from src import user_service
@@ -17,5 +18,14 @@ def get_friends(user_id: str):
     return user_service.get_friends(user_id)
 
 
+def get_openapi_spec():
+    return app.openapi()
+
+
 if __name__ == "__main__":
+    with open('../swagger.json', 'w') as file:
+        file.write(json.dumps(get_openapi_spec()))
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if sys.argv[1] == "openapi":
+    print(get_openapi_spec())
